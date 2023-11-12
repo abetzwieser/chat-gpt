e25275424f2fc7c197f354b28a69d2200f323040
ktiner1 <143346650+ktiner1@users.noreply.github.com>
2023-11-12 16:39:11 -0600

    encrypt_message

--- code/crypto.hpp
+++ code/crypto.hpp
@@ -3,7 +3,7 @@
 #define CRYPTO_HPP
 
 void generate_keypair(const char* user_password, unsigned char* salt, unsigned char* public_key, unsigned char* private_key);
-std::vector<unsigned char> encrypt_message(unsigned char* sendersk, unsigned char* recpk, const unsigned char* message_or_whatever, const unsigned char* nonce);
+const char* encrypt_message(unsigned char* sender_privk, unsigned char* rec_pubk, const char* message_or_whatever, SequentialNonce& nonceGen); 
 std::vector<unsigned char> decrypt_message(unsigned char* private_key, const unsigned char* sender_pk, const std::vector<unsigned char>& cipher,  const unsigned char* nonce);
 
 #endif /* CRYPTO_HPP */
