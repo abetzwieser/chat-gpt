ca862d93aa52c92f1507395ea24152809bfc1400
ktiner1 <143346650+ktiner1@users.noreply.github.com>
2023-11-12 17:20:20 -0600

    combination of nonce.hpp and crypto.hpp

--- code/crypto.hpp
+++ code/crypto.hpp
@@ -2,6 +2,19 @@
 #ifndef CRYPTO_HPP
 #define CRYPTO_HPP
 
+#include <sodium.h>
+#include <iostream>
+#include <string>
+
+class SequentialNonce {
+public:
+    SequentialNonce();
+    std::string getRandomNonce();
+
+private:
+    uint64_t counter;
+};
+
 void generate_keypair(const char* user_password, unsigned char* salt, unsigned char* public_key, unsigned char* private_key);
 const char* encrypt_message(unsigned char* sender_privk, unsigned char* rec_pubk, const char* message_or_whatever, SequentialNonce& nonceGen); 
 const char* decrypt_message(unsigned char* private_key, const unsigned char* sender_pubk, const char* cipher_with_nonce);
